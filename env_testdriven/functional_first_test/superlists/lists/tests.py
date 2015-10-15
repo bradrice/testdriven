cae346bfbf7f11443d3f1c92c2f4e5e372ed2a05
Brad Rice <bradrice1@gmail.com>
2015-10-14 20:11:30 -0400

    Add app for lists, with deliberitely failing unit test

--- env_testdriven/functional_first_test/superlists/lists/tests.py
+++ env_testdriven/functional_first_test/superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 
 class SmokeTest(TestCase):
-    def test_bad_match(selfself):
+	
+    def test_bad_maths(self):
         self.assertEqual(1 + 1, 3)
 
